4b316036406cc562453525a8cc5e218f3cf4b77f
Manish Kumar Prajapati <myindiaman@gmail.com>
2024-05-15 18:49:42 +0530

    added some basic tailwind details
    this checkin has been made to verify if react and tailwind styling both are working properly or not

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
+    <div className="w-full h-[100vh] flex gap-4 flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-teal-600">Manish Prajapati</h1>
+      <p className="text-md font-sans text-slate-500">Problem Solver</p>
+    </div>
   );
 }
 
