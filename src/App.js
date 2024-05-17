211dd8034c2ed8d2835d400cbaaeee8a701e386d
Manish Kumar Prajapati <myindiaman@gmail.com>
2024-05-17 19:52:33 +0530

    added routes to maintain 2 copies of resume
    added routes to maintain 2 copies of resume

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import HeroSection from "./Resume/Hero/HeroSection";
 import Navbar from "./Resume/Navbar/Navbar";
+import MainPage from "./Resume/Hero/MainPage";
 
 function App() {
   return (
-    <div className="w-full h-full bg-richblack-900 text-richblack-5">
-      <Navbar/>
-      <div className="py-4">
-        <HeroSection/>
-      </div>
-    </div>  
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<MainPage/>} /> 
+        </Routes>
+    </div> 
   );
 }
 
---
+++ src/Resume/Hero/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+
+const MainPage = () => {
+  return (
+    <div className="w-full h-full bg-richblack-900 text-richblack-5">
+      <div className="py-4">
+        <HeroSection/>
+      </div>
+    </div> 
+  )
+}
+
+export default MainPage
